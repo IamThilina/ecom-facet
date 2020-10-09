d536e6e6154d5724480e8db75983f72bf53ca7e0
Thilina Hettiarachchi <thilina@different.com.au>
2020-10-09 20:07:18 +0530

    Display selected categories as chips

--- src/components/selected-categories.js
+++ src/components/selected-categories.js
@@ -1,10 +1,16 @@
 import * as React from "react";
 import {useSelector} from 'react-redux'
+import Chip from "@material-ui/core/Chip";
 
 const SelectedCategories = () => {
     const selectedCategories = useSelector(({selectedCategories}) => selectedCategories);
 
-    return selectedCategories.map((category) => <div>{category.name}</div>)
+    return <div>
+        <h3> Selected Categories</h3>
+        {
+            selectedCategories.map((category) => <Chip label={category.name}/>)
+        }
+    </div>
 
 };
 
