583cd80a951e2e141c7a717dee100fb9abfce237
Thilina Hettiarachchi <thilina@different.com.au>
2020-10-10 23:37:55 +0530

    Added tests for selected-categories reducer

---
+++ src/reducers/selected-categories.spec.ts
@@ -0,0 +1,104 @@
+import deepFreeze from "deep-freeze";
+import selectedCategoriesReducer from "./selected-categories";
+import { SelectedCategories } from "../types";
+
+describe("selectedCategories reducer", () => {
+  it("handles ADD action", () => {
+    const state: SelectedCategories = [];
+    const action = {
+      type: "ADD",
+      payload: {
+        id: "001",
+        name: "Item-1",
+      },
+    };
+
+    deepFreeze(state);
+    deepFreeze(action);
+
+    const nextState = selectedCategoriesReducer(state, action);
+    expect(nextState).toEqual([
+      {
+        id: "001",
+        name: "Item-1",
+      },
+    ]);
+  });
+
+  it("handles REMOVE action", () => {
+    const state = [
+      {
+        id: "001",
+        name: "Item-1",
+      },
+    ];
+    const action = {
+      type: "REMOVE",
+      payload: {
+        id: "001",
+        name: "Item-1",
+      },
+    };
+
+    deepFreeze(state);
+    deepFreeze(action);
+
+    const nextState = selectedCategoriesReducer(state, action);
+    expect(nextState).toEqual([]);
+  });
+
+  it("handles REMOVE_ALL action", () => {
+    const state = [
+      {
+        id: "001",
+        name: "Item-1",
+      },
+      {
+        id: "002",
+        name: "Item-2",
+      },
+    ];
+    const action = {
+      type: "REMOVE_ALL",
+      payload: null,
+    };
+
+    deepFreeze(state);
+    deepFreeze(action);
+
+    const nextState = selectedCategoriesReducer(state, action);
+    expect(nextState).toEqual([]);
+  });
+
+  it("handles SELECT_ALL_SUB_CATEGORY action", () => {
+    const state: SelectedCategories = [];
+    const action = {
+      type: "SELECT_ALL_SUB_CATEGORY",
+      payload: [
+        {
+          id: "001",
+          name: "Item-1",
+        },
+        {
+          id: "002",
+          name: "Item-2",
+        },
+      ],
+    };
+
+    deepFreeze(state);
+    deepFreeze(action);
+
+    const nextState = selectedCategoriesReducer(state, action);
+    expect(nextState).toEqual([
+      {
+        id: "001",
+        name: "Item-1",
+      },
+      {
+        id: "002",
+        name: "Item-2",
+      },
+    ]);
+  });
+});
